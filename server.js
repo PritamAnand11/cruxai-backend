120eed7f6f8ea499c53e36d2598eb1b4f6e4d896
Pritam Anand <pritamanand60@gmail.com>
2026-08-12 08:11:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,10 +6,9 @@ dotenv.config();
 
 const app = express();
 
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`✅ Server running on http://localhost:5000`);
+  console.log(`Server running on http://localhost:5000`);
 });
 
 app.use(cors());
